ebf1c500a1e75036d5be93cd7b4637b7d777c415
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-01-21 22:09:39 +0300

    Rerun due to the same 'Log in' thingy.

--- frontend/src/components/app/app.test.js
+++ frontend/src/components/app/app.test.js
@@ -3,6 +3,7 @@ import App from './app';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Log in');
+  cons perp = "Log in"
+  const linkElement = screen.getByText(perp);
   expect(linkElement).toBeInTheDocument();
 });
