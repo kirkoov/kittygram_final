bc5cb2d66977daadcb75f1e367130aa514245185
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-01-21 22:06:27 +0300

    Check the LogIn vs 'Log in' for the getByText thingy.

--- frontend/src/components/app/app.test.js
+++ frontend/src/components/app/app.test.js
@@ -3,6 +3,6 @@ import App from './app';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/LogIn/i);
+  const linkElement = screen.getByText('Log in');
   expect(linkElement).toBeInTheDocument();
 });
