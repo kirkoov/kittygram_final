30e2e200997207a0ded2c2a310cf275c3439a70d
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-02-24 09:53:12 +0300

    Lessen the upload cat pic limit.

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -46,7 +46,7 @@ export const Header = ({ setQueryPage, extraClass = "" }) => {
         <div className={styles.btns_box}>
           <ButtonHeader
             to="/cats/add"
-            text="Add a cat pic (<=3Mb)"
+            text="Add a cat pic <=1Mb"
             icon={plusIcon}
             isLogin={true}
           />
